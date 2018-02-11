5860827c68732dfad9d9b5e24d70a48d441b3f06
agavaa <agatajedryszek@gmail.com>
2018-02-11 13:47:40 +0100

    set up observable functionality

--- core/src/com/pong/game/sprites/Ball.java
+++ core/src/com/pong/game/sprites/Ball.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.pong.game.MyPongGame;
 import com.pong.game.PositionObserver;
-import com.pong.game.ScoreObserver;
 
 import java.util.ArrayList;
 import java.util.List;
